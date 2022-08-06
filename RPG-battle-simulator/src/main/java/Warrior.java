d5c23c76ed5a07862d106734f020929962e7919b
1eyre <leyre.sanchez.f@gmail.com>
2022-08-06 11:52:49 +0200

    Add attributes

--- RPG-battle-simulator/src/main/java/Warrior.java
+++ RPG-battle-simulator/src/main/java/Warrior.java
@@ -1,2 +1,11 @@
-public class warrior {
+public class Warrior extends Character{
+    // stamina - number to represent a resource the warrior consumes to make an attack
+    // strength - number to calculate how strong the warrior attack is
+    public int stamina;
+    public int strength;
+
+    @Override
+    public void Attack() {
+
+    }
 }
