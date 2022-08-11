4bf34b8d91295aa7c151049789cd5c45c4ee28d2
Leyre <leyre.sanchez.f@gmail.com>
2022-08-11 19:13:58 +0200

    Add attack

--- RPG-battle-simulator/src/main/java/Wizard.java
+++ RPG-battle-simulator/src/main/java/Wizard.java
@@ -1,11 +1,23 @@
 public class Wizard extends Character{
-    // mana - number to represent a resource the wizard consumes to cast spells
-    // intelligence - number to calculate how strong the wizard spells are
     public int mana;
     public int intelligence;
 
+
+    public void Fireball(Character opponent){
+        if(this.mana > 0){
+            this.mana -= 5;
+            opponent.hp -= this.intelligence;
+        }
+    }
+
+    public void StaffHit(Character opponent){
+        if(this.mana > 0){
+            this.mana -= 1;
+            opponent.hp -= 2;
+        }
+    }
     @Override
     public void Attack() {
-
+  
     }
 }
