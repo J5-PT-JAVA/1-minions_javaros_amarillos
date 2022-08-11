2ddfff4a7ecce393f89e9e8342586ff9c59fd62c
Leyre <leyre.sanchez.f@gmail.com>
2022-08-11 21:34:22 +0200

    Add more todos

--- RPG-battle-simulator/src/main/java/Attacker.java
+++ RPG-battle-simulator/src/main/java/Attacker.java
@@ -3,7 +3,7 @@ public interface Attacker {
         /*TODO: Keep a detailed log (on the standard output) of every action taken during the duels.
         * Mantenga un registro detallado (en la salida estándar) de cada acción realizada durante los duelos.
         */
-
+        // TODO: Ambos atacan y reciben a la vez
         public void Attack(Character opponent);
 
 
--- RPG-battle-simulator/src/main/java/Parties.java
+++ RPG-battle-simulator/src/main/java/Parties.java
@@ -1,39 +1,61 @@
-public static class Party {
-
+public class Parties {
     public ArrayList<Character> fighter;
     public int size;
-    public void fillParty(){
-        // size has a random value between 2 and 10
-        this.size = (int) (Math.random() * 10) + 2;
-        for (int i = 0; i < this.size; i++) {
-            // Each party has random fighters (Warrior or Wizard)
-            if (Math.random() > 5) {
-                this.fighter.add(new Warrior(i, i +"- Warrior Jr"));
-            } else {
-                this.fighter.add(new Wizard(i, i+ "- Wizard Jr"));
-            }
-        }
+    public Parties(int size) {
+        this.size = size;
+        this.fighter = new ArrayList<Character>();
     }
-    // TODO: Show the graveyard/ Mostrar tanatorio
-    // TODO: Añadir funcion de guerreros en pie
-    // TODO: Import a party using a CSV file.
-    // TODO: Añadir tanatorio
 
+    public static void main(String[] args) {
+        Party party1 = new Party();
+        Party party2 = new Party();
+        party1.fillParty();
+        party2.fillParty();
+        party1.battle(party2);
+    }
 
+    public static class Party {
 
-    public void battle(Party otherParty) {
-        while (this.fighter.size() > 0 && otherParty.fighter.size() > 0) {
-            for (int i = 0; i < this.fighter.size(); i++) {
-                this.fighter.get(i).Attack(otherParty.fighter.get(i));
-            }
-            for (int i = 0; i < otherParty.fighter.size(); i++) {
-                otherParty.fighter.get(i).Attack(this.fighter.get(i));
+        public ArrayList<Character> fighter;
+        public int size;
+
+        /* Todo: Hay 3 maneras de crear los parties:
+        - Tamaño random con luchadores random
+        - Import por CSV
+        - Por inputs en pantalla
+        */
+        public void fillParty() {
+            // size has a random value between 2 and 10
+            this.size = (int) (Math.random() * 10) + 2;
+            for (int i = 0; i < this.size; i++) {
+                // Each party has random fighters (Warrior or Wizard)
+                if (Math.random() > 5) {
+                    this.fighter.add(new Warrior(i, i + "- Warrior Jr"));
+                } else {
+                    this.fighter.add(new Wizard(i, i + "- Wizard Jr"));
+                }
             }
         }
-        if (this.fighter.size() > 0) {
-            System.out.println("Party 1 wins");
-        } else {
-            System.out.println("Party 2 wins");
+        // TODO: Añadir funcion de guerreros en pie
+        // TODO: Añadir tanatorio
+        // TODO: When the battle is over the winner will return to the party and the loser will be removed and sent to the graveyard.
+        // Then you can choose the combatants for the next duel.
+        // TODO: When a party lose all their members a winner party is declared.
+
+        public void battle(Party otherParty) {
+            while (this.fighter.size() > 0 && otherParty.fighter.size() > 0) {
+                for (int i = 0; i < this.fighter.size(); i++) {
+                    this.fighter.get(i).Attack(otherParty.fighter.get(i));
+                }
+                for (int i = 0; i < otherParty.fighter.size(); i++) {
+                    otherParty.fighter.get(i).Attack(this.fighter.get(i));
+                }
+            }
+            if (this.fighter.size() > 0) {
+                System.out.println("Party 1 wins");
+            } else {
+                System.out.println("Party 2 wins");
+            }
         }
     }
 }
