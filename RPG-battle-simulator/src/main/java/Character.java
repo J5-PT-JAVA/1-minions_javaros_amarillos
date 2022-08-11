b374e83915ca6865b6d18c66346687f7a850dcec
Leyre <leyre.sanchez.f@gmail.com>
2022-08-11 20:22:44 +0200

    Add getters and setters

--- RPG-battle-simulator/src/main/java/Character.java
+++ RPG-battle-simulator/src/main/java/Character.java
@@ -1,14 +1,46 @@
 public abstract class Character implements Attacker {
-    //id - unique identifier
-    //name - String
-    //hp - number representing the health points
-    //isAlive - flag to determine if the Player is alive
     public int id;
-    // se puede eliminar, es para prueba
-    public int health;
     public String name;
     public int hp;
     public Boolean isAlive;
 
+    public Character(int id, String name, int hp) {
+        this.id = id;
+        this.name = name;
+        this.hp = hp;
+        this.isAlive = true;
+    }
 
+public void PrintStats() {
+    System.out.println("Name: " + this.name + " HP: " + this.hp);
+}
+
+    public abstract void Attack();
+
+    // add getters
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public Boolean getIsAlive() {
+        return isAlive;
+    }
+    // add setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public void setIsAlive(Boolean isAlive) {
+        this.isAlive = isAlive;
+    }
 }
