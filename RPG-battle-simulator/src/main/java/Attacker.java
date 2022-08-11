a3bbcecf30678a3120feb962dc7de3bda2e254f0
Leyre <leyre.sanchez.f@gmail.com>
2022-08-11 21:17:46 +0200

    Add todos

--- RPG-battle-simulator/src/main/java/Attacker.java
+++ RPG-battle-simulator/src/main/java/Attacker.java
@@ -1,5 +1,9 @@
 public interface Attacker {
 
+        /*TODO: Keep a detailed log (on the standard output) of every action taken during the duels.
+        * Mantenga un registro detallado (en la salida estándar) de cada acción realizada durante los duelos.
+        */
+
         public void Attack(Character opponent);
 
 
--- RPG-battle-simulator/src/main/java/Parties.java
+++ RPG-battle-simulator/src/main/java/Parties.java
@@ -1,42 +1,40 @@
-public class Parties {
-    public static void main(String[] args) {
-        Party party1 = new Party();
-        Party party2 = new Party();
-        party1.fillParty();
-        party2.fillParty();
-        party1.battle(party2);
-    }
+public static class Party {
 
-    public static class Party {
-        public ArrayList<Character> party = new ArrayList<Character>();
-        public void fillParty() {
-            Random random = new Random();
-            int partySize = random.nextInt(10) + 1;
-            for (int i = 0; i < partySize; i++) {
-                int randomNumber = random.nextInt(2);
-                if (randomNumber == 0) {
-                    party.add(new Warrior());
-                } else {
-                    party.add(new Wizard());
-                }
+    public ArrayList<Character> fighter;
+    public int size;
+    public void fillParty(){
+        // size has a random value between 2 and 10
+        this.size = (int) (Math.random() * 10) + 2;
+        for (int i = 0; i < this.size; i++) {
+            // Each party has random fighters (Warrior or Wizard)
+            if (Math.random() > 5) {
+                this.fighter.add(new Warrior(i, i +"- Warrior Jr"));
+            } else {
+                this.fighter.add(new Wizard(i, i+ "- Wizard Jr"));
             }
         }
-        public void battle(Party opponentParty) {
-            while (true) {
-                for (Character character : party) {
-                    character.Attack(opponentParty.party.get(0));
-                }
-                for (Character character : opponentParty.party) {
-                    character.Attack(party.get(0));
-                }
-                if (party.get(0).hp <= 0) {
-                    System.out.println("Party 2 wins");
-                    break;
-                } else if (opponentParty.party.get(0).hp <= 0) {
-                    System.out.println("Party 1 wins");
-                    break;
-                }
+    }
+    // TODO: Show the graveyard/ Mostrar tanatorio
+    // TODO: Añadir funcion de guerreros en pie
+    // TODO: Import a party using a CSV file.
+    // TODO: Añadir tanatorio
+
+
+
+    public void battle(Party otherParty) {
+        while (this.fighter.size() > 0 && otherParty.fighter.size() > 0) {
+            for (int i = 0; i < this.fighter.size(); i++) {
+                this.fighter.get(i).Attack(otherParty.fighter.get(i));
             }
+            for (int i = 0; i < otherParty.fighter.size(); i++) {
+                otherParty.fighter.get(i).Attack(this.fighter.get(i));
+            }
+        }
+        if (this.fighter.size() > 0) {
+            System.out.println("Party 1 wins");
+        } else {
+            System.out.println("Party 2 wins");
         }
     }
 }
+
